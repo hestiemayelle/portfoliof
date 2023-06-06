354c640d65c426fd2824de56d8022ab69a8616f0
hestiemayelle <mayellematho@gmail.com>
2023-06-06 17:51:50 +0100

    Created service routes

---
+++ backend/routes/serviceRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { getAllService, getSingleService, updateService, createService, deleteService } = require("../controllers/serviceController");
+
+const router = express.Router();
+
+// Get all Services
+router.get("/", getAllService);
+
+// Get a single Service
+router.get("/:id", getSingleService);
+
+// Update a Service
+router.patch("/:id", updateService);
+
+// Create a new Service 
+router.post("/:id", createService);
+
+// delete a Service 
+router.delete("/:id", deleteService);
+
+module.exports = router;
